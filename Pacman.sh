3da20e11a573433f76bb94193842e5dea8ba3566
3c01i <qapfuc@gmail.com>
2022-09-13 18:02:39 +0000

    1.0.2

--- Pacman.sh
+++ Pacman.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd
 echo "I install system program"
-sudo pacman -S base-devel git pipewire pipewire-pulse pipewire-jack xorg mesa-demos nvidia android-udev cmake
+sudo pacman -S base-devel git pipewire pipewire-pulse pipewire-jack xorg mesa-demos nvidia android-udev andoid-tools cmake
 echo "Success"
 read m
 echo "I install WM"
-sudo pacman -S gnome
+sudo pacman -S plasma
 echo "Success"
 read m
 echo "I activated system program"
@@ -19,5 +19,5 @@ echo "Open multilib in /etc/pacman.conf"
 read m
 sudo pacman -Sy
 echo "I install favorite program"
-sudo pacman -S djvulibre rclone gnome-tweaks telegram-desktop fish thunderbird vlc audacity blender discord firefox htop inkscape kdenlive krita neofetch obs-studio qbittorrent scrcpy steam 
+sudo pacman -S dolphin alacritty djvulibre rclone telegram-desktop fish thunderbird vlc audacity blender discord firefox htop inkscape kdenlive krita neofetch obs-studio qbittorrent scrcpy steam
 echo "Success"
