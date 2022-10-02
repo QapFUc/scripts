234c8b3cceac65f60feb195823e304450368a64f
3c01i <qapfuc@gmail.com>
2022-10-02 10:19:50 +0000

    1.0.2

--- Pacman.sh
+++ Pacman.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 cd
+read m
+echo "Open multilib in /etc/pacman.conf"
+read m
+sudo pacman -Sy
 echo "I install system program"
 sudo pacman -S base-devel git amd-ucode pipewire pipewire-pulse pipewire-jack xorg mesa-demos nvidia android-udev cmake gcc ligda grub-customizer
 sudo pacman -S wine wine-mono wine-gecko
@@ -15,11 +19,8 @@ sudo systemctl enable NetworkManager
 sudo systemctl start NetworkManager
 sudo pacman -Sy
 echo "Success"
-read m
-echo "Open multilib in /etc/pacman.conf"
-read m
 sudo pacman -Sy
 echo "I install favorite program"
-sudo pacman -S rclone gnome-bluetooth gnome-tweaks telegram-desktop fish thunderbird vlc audacity blender discord firefox htop inkscape kdenlive krita gimp neofetch obs-studio qbittorrent scrcpy steam lutris
+sudo pacman -S rclone gnome-bluetooth gnome-tweaks telegram-desktop fish thunderbird vlc audacity blender discord firefox htop inkscape kdenlive krita gimp neofetch obs-studio qbittorrent scrcpy steam
 sudo systemctl enable bluetooth.service 
 echo "Success"
